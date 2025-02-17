45aee284529077d4ba683cfec5be86a4b84365af
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-17 23:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
     //Write your code here
+	let form = document.querySelector('form');
+	let fName = document.getElementById('fname');
+	let lName = document.getElementById('lName');
 
+	form.addEventListener("submit" , (e)=>{
+		e.preventDefault();
+
+		let firstName = fName.value;
+		let lastName = lName.value;
+
+		if(firstName.length > 0 && lastName > 0){
+			alert(`${firstName} ${lastName}`);
+		}
+	})
 }
